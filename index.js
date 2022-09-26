cecac06dc930f174934beb2089232220e490e09a
Moringa School <Leennyambura210@gmail.com>
2022-09-26 10:02:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (1900 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+}
+function ternaryCheckCity(city){
+  // write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  // write your code here!
+switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+    case 'not as generous':
+    return 'Thank you.';
+    default:
+      return 'Bye.'
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
 }
